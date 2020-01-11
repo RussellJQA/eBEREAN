3208c5bc5dddc93af36b187f34a7b93a5069dc92
Russell Johnson <16086475+RussellJQA@users.noreply.github.com>
2020-01-11 13:40:24 -0500

    Move Psalm readings to separate file

--- WeekendPsalms.py
+++ WeekendPsalms.py
@@ -1,5 +1,8 @@
 """
-Create a Bible reading plan with readings from the Psalms for each weekend day.
+Create a Bible reading plan with 104 readings from the Psalms, 1 for each weekend day.
+Although the readings themselves are taken directly from the Bible Reading Plan of
+Robert Murray M‘Cheyne (1813-1843), this plan orders them quite differently.
+
 """
 
 import calendar
@@ -7,119 +10,13 @@ import datetime
 import os
 
 from bible_books import bible_books
+from rmm_psalm_readings import rmm_original_104_psalm_readings
 from create_bible_plan import create_plan_with_playlists, get_weekday, process_reading
 
 bible_books_list = list(bible_books.keys())
 
 
 def WeekendPsalms(daily_readings, year):  # Weekend Worship (Psalms)
-
-    weekend_psalm_readings = [
-        "1-2",
-        "3-4",
-        "5-6",
-        "7-8",
-        "9",
-        "10",
-        "11-12",
-        "13-14",
-        "15-16",
-        "17",
-        "18",
-        "19",
-        "20-21",
-        "22",
-        "23-24",
-        "25",
-        "26-27",
-        "28-29",
-        "30",
-        "31",
-        "32",
-        "33",
-        "34",
-        "35",
-        "36",
-        "37",
-        "38",
-        "39",
-        "40-41",
-        "42-43",
-        "44",
-        "45",
-        "46-47",
-        "48",
-        "49",
-        "50",
-        "51",
-        "52-54",
-        "55",
-        "56-57",
-        "58-59",
-        "60-61",
-        "62-63",
-        "64-65",
-        "66-67",
-        "68",
-        "69",
-        "70-71",
-        "72",
-        "73",
-        "74",
-        "75-76",
-        "77",
-        "78:1-37",
-        "78:38-72",
-        "79",
-        "80",
-        "81-82",
-        "83-84",
-        "85",
-        "86-87",
-        "88",
-        "89",
-        "90",
-        "91",
-        "92-93",
-        "94",
-        "95-96",
-        "97-98",
-        "99-101",
-        "102",
-        "103",
-        "104",
-        "105",
-        "106",
-        "107",
-        "108-109",
-        "110-111",
-        "112-113",
-        "114-115",
-        "116",
-        "117-118",
-        "119:1-24",
-        "119:25-48",
-        "119:49-72",
-        "119:73-96",
-        "119:97-120",
-        "119:121-144",
-        "119:145-176",
-        "120-122",
-        "123-125",
-        "126-128",
-        "129-131",
-        "132-134",
-        "135-136",
-        "137-138",
-        "139",
-        "140-141",
-        "142-143",
-        "144",
-        "145",
-        "146-147",
-        "148",
-        "149-150",
-    ]
 
     date = datetime.datetime(year, 1, 1)  # January 1
     while get_weekday(date) not in ("Sat", "Sun"):
@@ -135,7 +32,7 @@ def WeekendPsalms(daily_readings, year):  # Weekend Worship (Psalms)
     #     extra_readings += 1  # Increment if leap year & December 30 is on a weekend
     # print(f'For {year}, {extra_readings} extra WeekendPsalms() readings are needed.')
 
-    for count, psalm_ref in enumerate(weekend_psalm_readings):
+    for count, psalm_ref in enumerate(rmm_original_104_psalm_readings):
         datedelta = 6 if (count % 2) else 1
         # If date is a Lord's Day, then increment date to the following Saturday
         # Else {date is a Saturday} increment date to the next day (a Lord's Day)
