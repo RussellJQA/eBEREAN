4d2755cc7904a2dbd870062883b97203204fa702
Russell Johnson <16086475+RussellJQA@users.noreply.github.com>
2020-02-06 18:08:00 -0500

    Add function build_frequency_lists(frequency) to use with Bible-level and chapter-level lists

--- create_bible_metadata.py
+++ create_bible_metadata.py
@@ -24,72 +24,112 @@ import os.path
 import re
 
 
+def key_value(element):
+    sort_key = (element[0], element[1])
+    return sort_key
+
+
+def desc_value_asc_key(element):
+    sort_key = (-1 * element[1], element[0])
+    return sort_key
+
+
+def build_frequency_lists(frequency):
+    total_words = 0  # The final value of total_words is 790,663
+    words_with_this_frequency = []
+    frequency_lists = {}
+    prev_occurrences = 0
+    occurrences = 0
+    for element in sorted(frequency.items(), key=desc_value_asc_key):
+        # Split into lists of words for each frequency:
+        word = element[0]
+        occurrences = element[1]  # For "the", occurrences is 64016
+        total_words += occurrences
+        if prev_occurrences and occurrences != prev_occurrences:
+            frequency_lists[prev_occurrences] = words_with_this_frequency[:]
+            words_with_this_frequency.clear()
+        words_with_this_frequency.append(word)
+        prev_occurrences = occurrences
+    frequency_lists[occurrences] = words_with_this_frequency[:]
+    frequency_lists = {total_words: ["TOTAL WORDS"], **frequency_lists}
+
+    total_words2 = 0  # Essentially, recalc total_words a 2nd way, for comparison.
+    for key, value in sorted(frequency_lists.items(), reverse=True):
+        if value != ["TOTAL WORDS"]:
+            total_words2 += int(key) * len(value)
+            # Increment by number of occurrences * number of words with that number
+    if total_words != total_words2:
+        print(f"total_words ({total_words}) != to total_words2 ({total_words2})")
+
+    return frequency_lists
+
+
+def build_frequency_lists(frequency):
+    total_words = 0  # The final value of total_words is 790,663
+    words_with_this_frequency = []
+    frequency_lists = {}
+    prev_occurrences = 0
+    occurrences = 0
+    for element in sorted(frequency.items(), key=desc_value_asc_key):
+        # Split into lists of words for each frequency:
+        word = element[0]
+        occurrences = element[1]  # For "the", occurrences is 64016
+        total_words += occurrences
+        if prev_occurrences and occurrences != prev_occurrences:
+            frequency_lists[prev_occurrences] = words_with_this_frequency[:]
+            words_with_this_frequency.clear()
+        words_with_this_frequency.append(word)
+        prev_occurrences = occurrences
+    frequency_lists[occurrences] = words_with_this_frequency[:]
+    frequency_lists = {total_words: ["TOTAL WORDS"], **frequency_lists}
+
+    total_words2 = 0  # Essentially, recalc total_words a 2nd way, for comparison.
+    for key, value in sorted(frequency_lists.items(), reverse=True):
+        if value != ["TOTAL WORDS"]:
+            total_words2 += int(key) * len(value)
+            # Increment by number of occurrences * number of words with that number
+    if total_words != total_words2:
+        print(f"total_words ({total_words}) != to total_words2 ({total_words2})")
+
+    return frequency_lists
+
+
+def write_word_frequency_files(word_frequency, word_frequency_lists_chapters):
+
+    # Write dict of KJV words, each paired (in a list) with its # of occurrences
+    # {["a", 8282], ["aaron", 350], ["aaronites", 2], ... ["zuzims", 1]}
+    word_frequency_sorted = {}
+    for key, value in sorted(word_frequency.items()):
+        word_frequency_sorted[key] = value
+    with open(r"BibleMetaData\word_frequency.json", "w") as write_file:
+        json.dump(word_frequency_sorted, write_file)
+
+    word_frequency_lists = build_frequency_lists(word_frequency)
+    with open(r"BibleMetaData\word_frequency_lists.json", "w") as write_file:
+        json.dump(word_frequency_lists, write_file, indent=4)
+
+    with open(r"BibleMetaData\word_frequency_lists_chapters.json", "w") as write_file:
+        json.dump(word_frequency_lists_chapters, write_file, indent=4)
+
+
 def main():
-    def key_value(element):
-        sort_key = (element[0], element[1])
-        return sort_key
-
-    def desc_value_asc_key(element):
-        sort_key = (-1 * element[1], element[0])
-        return sort_key
-
-    def build_word_frequency_lists(word_frequency, word_frequency_all_chapters):
-
-        # Write dict of KJV words, each paired (in a list) with its # of occurrences
-        # {["a", 8282], ["aaron", 350], ["aaronites", 2], ... ["zuzims", 1]}
-        word_frequency_sorted = {}
-        for key, value in sorted(word_frequency.items()):
-            word_frequency_sorted[key] = value
-        with open(r"BibleMetaData\word_frequency.json", "w") as write_file:
-            json.dump(word_frequency_sorted, write_file)
-
-        with open(r"BibleMetaData\word_frequency_all_chapters.json", "w") as write_file:
-            json.dump(word_frequency_all_chapters, write_file, indent=4)
-
-        total_words = 0  # The final value of total_words is 790,663
-        words_with_this_frequency = []
-        word_frequency_lists = {}
-        prev_occurrences = 0
-        occurrences = 0
-        for element in sorted(word_frequency.items(), key=desc_value_asc_key):
-            # Split into lists of words for each frequency:
-            word = element[0]
-            occurrences = element[1]  # For "the", occurrences is 64016
-            total_words += occurrences
-            if prev_occurrences and occurrences != prev_occurrences:
-                word_frequency_lists[prev_occurrences] = words_with_this_frequency[:]
-                words_with_this_frequency.clear()
-            words_with_this_frequency.append(word)
-            prev_occurrences = occurrences
-        word_frequency_lists[occurrences] = words_with_this_frequency[:]
-        word_frequency_lists = {total_words: ["TOTAL WORDS"], **word_frequency_lists}
-
-        total_words2 = 0  # Essentially, recalc total_words a 2nd way, for comparison.
-        for key, value in sorted(word_frequency_lists.items(), reverse=True):
-            if value != ["TOTAL WORDS"]:
-                total_words2 += int(key) * len(value)
-                # Increment by number of occurrences * number of words with that number
-        if total_words != total_words2:
-            print(f"total_words ({total_words}) != to total_words2 ({total_words2})")
-
-        with open(r"BibleMetaData\word_frequency_lists.json", "w") as write_file:
-            json.dump(word_frequency_lists, write_file, indent=4)
-
     book_abbrevs = {}
     verse_counts_by_chapter = {}  # dict of verse counts, indexed by chapter
     # e.g., dict["Gen 1"]=31
     verse_counts_by_count = {}  # dict of full_refs, indexed by verse counts
     verse_counts_by_desc_count = {}  # above dict, sorted by decreasing verse count
     word_frequency = {}
+    word_frequency_lists_chapters = {}
     word_frequency_this_chapter = {}
-    word_frequency_all_chapters = {}
 
     script_dir = os.path.dirname(os.path.realpath(__file__))
     source_files = os.path.join(script_dir, "downloads", "kjv_chapter_files")
     kjv_chapter_files = sorted(glob.glob(os.path.join(source_files, "*.txt")))
     # sorted() because glob() may return the list in an arbitrary order
 
+    chapter_count = 0
     for chapter_file in kjv_chapter_files:
+        chapter_count += 1
         read_file = open(chapter_file, "r", encoding="utf-8")
         lines = read_file.readlines()
         # There's no need to exclude the blank line at the end of chapter files,
@@ -121,8 +161,9 @@ def main():
         else:
             verse_counts_by_count[verse_count] = [full_ref]
 
+        # TODO: Put this into a function
         # Calculate word frequencies
-        word_frequency_this_chapter = {}
+        frequency_this_chapter = {}
         for line in lines[2:]:
             line = re.sub("[¶’]\S*", "", line).strip()
             # Eliminate paragraph markers, possessives, and leading/trailing blanks
@@ -134,16 +175,15 @@ def main():
                     word_frequency[word_lower] += 1
                 else:
                     word_frequency[word_lower] = 1
-                if word_lower in word_frequency_this_chapter:
-                    word_frequency_this_chapter[word_lower] += 1
+                if word_lower in frequency_this_chapter:
+                    frequency_this_chapter[word_lower] += 1
                 else:
-                    word_frequency_this_chapter[word_lower] = 1
+                    frequency_this_chapter[word_lower] = 1
 
-        word_frequency_all_chapters[full_ref] = sorted(
-            word_frequency_this_chapter.items()
-        )
+        frequency_lists_this_chapter = build_frequency_lists(frequency_this_chapter)
+        word_frequency_lists_chapters[full_ref] = frequency_lists_this_chapter
 
-    build_word_frequency_lists(word_frequency, word_frequency_all_chapters)
+    write_word_frequency_files(word_frequency, word_frequency_lists_chapters)
 
     with open(r"BibleMetaData\book_abbreviations.json", "w") as write_file:
         json.dump(book_abbrevs, write_file, indent=4)
