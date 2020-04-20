713dfdd8e5aa3dbe21c1f47650f15053be0758a9
Russell Johnson <16086475+RussellJQA@users.noreply.github.com>
2020-04-20 17:16:55 -0400

    Fuller/better use of string.Templates

--- chapter_word_freq_html.py
+++ chapter_word_freq_html.py
@@ -1,15 +1,23 @@
 import string
 
-BEGINNING = """<!DOCTYPE html>
-<html lang="en">
+ENDING = """
+        </tbody>
+    </table>
+</body>
+
+</html>
+"""
+
+beginning = string.Template(
+    """<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    """
-
-HEAD_END = """
-    <!-- The CSS in this style tag is from https://www.w3schools.com/html/html_tables.asp -->
+    <title>$key - Word Frequencies</title>
+    <h1>$key - Word Frequencies</h1>
+    <h2>TOTAL (Gen 1),797,$words_in_bible</h2>
+     <!-- The CSS in this style tag is from https://www.w3schools.com/html/html_tables.asp -->
     <style>
         table,
         th,
@@ -22,7 +30,6 @@ HEAD_END = """
             padding: 15px;
         }
     </style>
-
 </head>
 
 <body>
@@ -30,7 +37,7 @@ HEAD_END = """
     https://stackoverflow.com/questions/10683712/html-table-sort/51648529 -->
     <script src="https://www.kryogenix.org/code/browser/sorttable/sorttable.js"></script>
 
-    <!-- Inititial table generated using http://convertcsv.com/csv-to-html.htm -->
+    <!-- Prototype table generated using http://convertcsv.com/csv-to-html.htm -->
 
     <!-- I removed the Bootstrap classes from the generated table. -->
     <!-- <table class="table table-bordered table-hover table-condensed"> -->
@@ -44,15 +51,8 @@ HEAD_END = """
                 <th title="Field #4">simpleRelFreq</th>
             </tr>
         </thead>
-        <tbody>
-"""
-
-ENDING = """        </tbody>
-    </table>
-</body>
-
-</html>
-"""
+        <tbody>"""
+)
 
 table_row = string.Template(
     """
@@ -62,8 +62,7 @@ table_row = string.Template(
                 <td align='right'>$numInKjv</td>
                 <td>$weightedRelFreq</td>
                 <td>$simpleRelFreq</td>
-            </tr>
-"""
+            </tr>"""
 )
 
 
@@ -71,11 +70,11 @@ def write_html_file(words_in_bible, key, html_fn, relative_word_frequency):
 
     with open(html_fn, "w") as write_file:
 
-        write_file.write(BEGINNING)
-        write_file.write(f"<title>{key} - Word Frequencies</title>")
-        write_file.write(f"<h1>{key} - Word Frequencies</h1>")
-        write_file.write(f"<h2>TOTAL (Gen 1),797,{words_in_bible}</h2>")
-        write_file.write(HEAD_END)
+        values = {
+            "key": key,
+            "words_in_bible": words_in_bible,
+        }
+        write_file.write(beginning.substitute(values))
 
         for count, chapter_word_freq in enumerate(relative_word_frequency):
             values = relative_word_frequency[chapter_word_freq]
